41358b3c5cbcc22f417c0f71909ee5df0b120e25
Matt Etress <mattetress@me.com>
2018-10-12 07:08:14 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -15,7 +15,8 @@ class MusicLibraryController
     puts "To play a song, enter 'play song'."
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
-    gets.chomp
+    input = gets.chomp
+    self.call unless input = "exit"
   end
 
 
