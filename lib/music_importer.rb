c347796fea6b2a984d7c45c6947d369deee1ae9e
Matt Etress <mattetress@me.com>
2018-10-12 05:20:41 +0000

    automatically backed up by learn

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -1,3 +1,18 @@
 class MusicImporter
+  attr_reader :path
+
+  def initialize(path)
+    @path = path
+  end
+
+  def files
+    file_list = Dir["#{path}/*.mp3"]
+    filenames = []
+    file_list.each do |file|
+      parts = file.split("/")
+      filenames << parts[parts.size - 1]
+    end
+    filenames
+  end
 
 end
