e041e29c5a5c297f71367a493d0af376fa31e654
Matt Etress <mattetress@me.com>
2018-10-12 04:47:18 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,12 @@
 class Song
   attr_accessor :name
-  attr_reader :artist
+  attr_reader :artist, :genre
   @@all = []
 
-  def initialize(name, artist = nil)
+  def initialize(name, artist = nil, genre = nil)
     @name = name
     self.artist = artist if artist
+    self.genre = genre if genre
   end
 
   def self.all
@@ -29,6 +30,11 @@ class Song
   def artist=(artist)
     @artist = artist
     artist.add_song(self)
+  end
+
+  def genre=(genre)
+    @genre = genre
+    genre.add_song(self) unless genre.songs.include?(self)
   end
 
 
