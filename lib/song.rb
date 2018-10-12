f01081211b5757c10589f966330e995e06856c6e
Matt Etress <mattetress@me.com>
2018-10-12 05:55:05 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -50,7 +50,7 @@ class Song
     song_name =  filename.split(" - ")[1]
     genre_name = filename.split(" - ")[2].gsub(".mp3", "")
     song = self.new(song_name)
-    Artist.find_or_create_by_name(artist_name)
+    Artist.find_or_create_by_name(filename.split(" - ")[0])
     Genre.find_or_create_by_name(genre_name)
     song
 
